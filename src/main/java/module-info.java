7724cfb65e6ab1892f703e8b0455c5c0bcc35138
Ferdinando Villa <ferdinando.villa@gmail.com>
2026-04-16 10:44:13 +0200

    Reorder module dependencies and comment out unused kim.ide dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,15 @@
 module org.integratedmodelling.klabeditor {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
     requires jdk.jsobject;
-
     requires org.kordamp.ikonli.javafx;
-    requires javafx.graphics;
     requires jdk.httpserver;
     requires org.integratedmodelling.languages.kim.ide;
     requires org.integratedmodelling.languages.kim.ide.org.eclipse.lsp4j;
     requires com.fasterxml.jackson.databind;
     requires java.desktop;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
 
     opens org.integratedmodelling.klabeditor to javafx.fxml;
     exports org.integratedmodelling.klabeditor;
