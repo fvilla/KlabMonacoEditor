45a36f52ba86bc704cff9365ac4ea84ab0b2f73e
Enrico Girotto <enrico.girotto@gmail.com>
2026-04-16 10:58:13 +0200

    Remove `languages.kim.ide` dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,14 +2,13 @@ module org.integratedmodelling.klabeditor {
     requires jdk.jsobject;
     requires org.kordamp.ikonli.javafx;
     requires jdk.httpserver;
-    requires org.integratedmodelling.languages.kim.ide;
-    requires org.integratedmodelling.languages.kim.ide.org.eclipse.lsp4j;
     requires com.fasterxml.jackson.databind;
     requires java.desktop;
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires org.eclipse.lsp4j;
 
     opens org.integratedmodelling.klabeditor to javafx.fxml;
     exports org.integratedmodelling.klabeditor;
